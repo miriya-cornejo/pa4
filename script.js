b2d4ef1bc2dc5c573c61979986a03071f638ecf1
Lisbeth Miriya Cornejo Vásquez <miriya.cornejov@gmail.com>
2025-11-24 13:47:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.slider').bxSlider({
+        auto: true,
+        pause: 3000,
+        controls: false,
+        pager: true,
+        mode: 'fade',
+        speed: 800
+    });
+});
